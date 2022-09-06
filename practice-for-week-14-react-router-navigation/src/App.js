53b8f600bf148fec5f37254a0a50306bbfd0f854
George Merida <gmerida92@gmail.com>
2022-09-05 22:06:19 -0700

    Still working on react router navigation

--- practice-for-week-14-react-router-navigation/src/App.js
+++ practice-for-week-14-react-router-navigation/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { NavLink, Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
@@ -7,6 +7,24 @@ function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
+      <nav className='comp nav' >
+        <ul>
+          <li>
+            <a href='/'>Anchor</a>  {/*when you have an anchor, and click the link, the entire page refreshes. Not the case with a link.*/}
+          </li>
+          <li>
+            <NavLink activeClassName='purple' activeStyle={{ fontWeight: 'bold' }} exact to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName='purple' activeStyle={{ fontWeight: 'bold' }} to='/stocks'>Stocks</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName='purple' activeStyle={{ fontWeight: 'bold' }} to='/Movies'>Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+
       <Switch>
         <Route exact path='/'>
           <Home />
@@ -16,6 +34,9 @@ function App() {
         </Route>
         <Route path='/movies'>
           <Movies />
+        </Route>
+        <Route path='/not-logged-in'>
+          <h1>You Must Be Logged In To Enter</h1>
         </Route>
         <Route>
           <h1>Page Not Found</h1>
--- practice-for-week-14-react-router-navigation/src/components/Stocks/index.js
+++ practice-for-week-14-react-router-navigation/src/components/Stocks/index.js
@@ -1,4 +1,13 @@
+import {Redirect} from 'react-router-dom'
+
 function Stocks() {
+  const loggedIn = true;
+  if(!loggedIn) {
+    return(
+      <Redirect to='/not-logged-in'></Redirect>
+    );
+  }
+
   return (
     <div className='comp orange'>
       <h1>Stocks Component</h1>
