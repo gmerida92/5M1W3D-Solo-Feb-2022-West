1a6a0ccf0077fa4c19fe0e8faf1b53803530f57e
George Merida <gmerida92@gmail.com>
2022-09-08 19:43:44 -0700

    React Router Nesting Complete

--- practice-for-week-14-react-router-nesting/src/components/MovieDetails/index.js
+++ practice-for-week-14-react-router-nesting/src/components/MovieDetails/index.js
@@ -1,7 +1,15 @@
-function MovieDetails() {
+import { useParams } from 'react-router-dom';
+
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+  console.log(movieId);
+
+  const movieChoice = movies.find((movieObj) => {return movieObj.id === parseInt(movieId, 10) });
+
   return (
     <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+      <h1>{movieChoice.title}</h1>
+      <p>{movieChoice.description}</p>
     </div>
   );
 }
--- practice-for-week-14-react-router-nesting/src/components/Movies/index.js
+++ practice-for-week-14-react-router-nesting/src/components/Movies/index.js
@@ -1,7 +1,23 @@
-function Movies() {
+import { Route, Switch, NavLink } from 'react-router-dom';
+import MovieDetails from '../MovieDetails';
+
+function Movies({ movies }) {
   return (
     <div className='comp orange'>
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movieObj) => {
+          return (
+            <NavLink style={{padding:'10px'}}key={movieObj.id} to={`/movies/${movieObj.id}`}>{movieObj.title}</NavLink>
+          )
+        })}
+      </nav>
+
+      <Switch>
+        <Route path='/movies/:movieId'>
+          <MovieDetails movies={movies}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
