7f4bd5905f5b5872dd9cde3a27dd96375e807ed3
George Merida <gmerida92@gmail.com>
2022-09-08 20:33:41 -0700

    Hooks useState Complete

--- practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
+++ practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
@@ -1,7 +1,29 @@
+import {useState} from 'react';
+import './UseState.css';
+
 const UseState = () => {
+  const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState(0);
+
+  const themeHandler = () => {
+    if(theme === 'light') {
+      return 'dark';
+    }
+    else {return 'light'}
+  }
+
   return (
-    <div>
+    <div className={`${theme} state`}>
       <h1>UseState Component</h1>
+      <button onClick={() => {return setTheme(themeHandler())}}>Change Theme</button>
+      {/* <button onClick={() => {return setTheme('dark')}}>Dark</button>
+      <button onClick={() => {return setTheme('light')}}>Light</button> */}
+      
+      <h1>{count}</h1>
+      {/* <button onClick = {() => {return setCount(count + 1 )}}>Increment</button> */}
+      <button onClick = {() => {return setCount((prevCount) => prevCount + 1 )}}>Increment</button>
+      {/* <button onClick = {() => {return setCount(count - 1)}}>Decrement</button> */}
+      <button onClick = {() => {return setCount((prevCount) => prevCount - 1 )}}>Decrement</button>
     </div>
   );
 };
